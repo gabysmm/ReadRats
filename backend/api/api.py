2bfc9e3343df956f47361999b4a65d4958306d3d
gabysmm <gabymagalhaes.sm@gmail.com>
2026-04-03 21:45:20 +0000

    chore: configurar autenticação JWT global

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,8 +1,9 @@
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
+from ninja_jwt.authentication import JWTAuth
 from .users.routers import router as users_router
 
-api = NinjaExtraAPI(title="ReadRats API")
+api = NinjaExtraAPI(title="ReadRats API", auth=JWTAuth())
 api.register_controllers(NinjaJWTDefaultController)
 
 api.add_router("/users/", users_router)
