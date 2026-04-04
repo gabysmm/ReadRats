05011e8c622eb18628209bfefa421434852d7bfb
gabysmm <gabymagalhaes.sm@gmail.com>
2026-04-04 18:30:41 +0000

    feat: adicionar models membership

--- backend/apps/community/models.py
+++ backend/apps/community/models.py
@@ -5,8 +5,18 @@ class Community(models.Model):
         ('temporary', 'Temporary'),
         ('continuous', 'Continuous')
     ]
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    type_community = models.CharField(max_length=20, choices=TYPE_CHOICES)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     creator = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True, related_name='communities')
     create_at = models.DateField(auto_now_add=True)
+
+class Membership(models.Model):
+    ROLE_CHOICE = [
+        ('admin', 'Admin'),
+        ('normal', 'Normal')
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='memberships')
+    community = models.ForeignKey('Community', on_delete=models.CASCADE, related_name='memberships')
+    input_date = models.DateField(auto_now_add=True)
